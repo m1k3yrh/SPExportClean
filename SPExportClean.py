ace640ab555791cf5aa03743716be50b5688bd0c
mike.rhodes <mike.rhodes@accenture.com>
2015-09-21 15:05:15 +0100

    Simplified
    Simplified to a single rule.

--- SPExportClean.py
+++ SPExportClean.py
@@ -43,13 +43,12 @@ except IOError:
 
 # Create Reg Exp
 regexp=re.compile('#[0-9;#]+')
-regexp2=re.compile(';+')
     
 for r in raw_data: # for each row
     print(r)
     
     for i in r: # for each cell on the row
-        out_file.write(regexp2.sub('; ',regexp.sub('',i))+'\t')
+        out_file.write(regexp.sub(' ',i)+'\t')
     
     out_file.write('\n') # End of record
     
